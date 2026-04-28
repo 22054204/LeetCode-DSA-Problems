ab5ad6b470fb8006bcb4a122fe7b12862205ce01
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-04-28 21:54:15 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (61.48%) - LeetSync

--- 4290-valid-elements-in-an-array/valid-elements-in-an-array.java
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.java
@@ -1,52 +1,33 @@
 class Solution {
     public List<Integer> findValidElements(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-
-        if(nums.length==1){
-            list.add(nums[0]);
-            return list;
+        int n = nums.length;
+        List<Integer> res = new ArrayList<>();
+        res.add(nums[0]);
+        int[] left = new int[n];
+        int maxele = nums[0];
+        for(int i = 1; i < n - 1; i++){
+            if(nums[i] > maxele){
+                left[i]++;
+            }
+            maxele = Math.max(maxele, nums[i]);
         }
-        if(nums.length==2){
-            list.add(nums[0]);
-            list.add(nums[1]);
-            return list;
+        int[] right = new int[n];
+        int maxright = nums[n - 1];
+
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] > maxright){
+                right[i]++;
+            }
+            maxright = Math.max(maxright, nums[i]);
         }
-
-        boolean[] valid = new boolean[nums.length];
-
-        start(nums, valid);  // left check
-        end(nums, valid);    // right check
-
-        for(int i=0;i<nums.length;i++){
-            if(valid[i]){
-                list.add(nums[i]);
+        for(int i = 1; i < n - 1; i++){
+            if(left[i] > 0 || right[i] > 0){
+                res.add(nums[i]);
             }
         }
-
-        return list;
-    }
-
-    public static void start(int[] nums, boolean[] valid){
-        int max = nums[0];
-        valid[0] = true;
-
-        for(int i=1;i<nums.length;i++){
-            if(nums[i] > max){
-                valid[i] = true;
-            }
-            max = Math.max(max, nums[i]);
+        if(n != 1){
+            res.add(nums[n - 1]);
         }
-    }
-
-    public static void end(int[] nums, boolean[] valid){
-        int max = nums[nums.length-1];
-        valid[nums.length-1] = true;
-
-        for(int i=nums.length-2;i>=0;i--){
-            if(nums[i] > max){
-                valid[i] = true;
-            }
-            max = Math.max(max, nums[i]);
-        }
+        return res;
     }
 }

5d17aab6302d7513dce2fcd8b7058a2c98e69e66
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-04-28 22:43:17 +0530

    Time: 86 ms (24.84%) | Memory: 47.5 MB (89.09%) - LeetSync

--- 4282-sort-vowels-by-frequency/sort-vowels-by-frequency.java
+++ 4282-sort-vowels-by-frequency/sort-vowels-by-frequency.java
@@ -1,61 +1,47 @@
+import java.util.*;
+
 class Solution {
     public String sortVowels(String s) {
-        int a = 0, e = 0, i = 0, o = 0, u = 0;
+        List<Character> list = new ArrayList<>();
+        Map<Character, Integer> firstIndex = new HashMap<>();
+        Map<Character, Integer> freq = new HashMap<>();
 
-        for(int k=0;k<s.length();k++){
-            char ch = s.charAt(k);
-            if(ch=='a') a++;
-            else if(ch=='e') e++;
-            else if(ch=='i') i++;
-            else if(ch=='o') o++;
-            else if(ch=='u') u++;
-        }
+        // collect + count + first index
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (isVowel(ch)) {
+                list.add(ch);
 
-        int fa = -1, fe = -1, fi = -1, fo = -1, fu = -1;
+                freq.put(ch, freq.getOrDefault(ch, 0) + 1);
 
-        for(int k=0;k<s.length();k++){
-            char ch = s.charAt(k);
-            if(ch=='a' && fa==-1) fa = k;
-            else if(ch=='e' && fe==-1) fe = k;
-            else if(ch=='i' && fi==-1) fi = k;
-            else if(ch=='o' && fo==-1) fo = k;
-            else if(ch=='u' && fu==-1) fu = k;
-        }
-
-        StringBuilder vowelOrder = new StringBuilder();
-
-        for(int t=0;t<5;t++){
-            if(a>0 && (a>e || (a==e && fa<fe)) && (a>i || (a==i && fa<fi)) && (a>o || (a==o && fa<fo)) && (a>u || (a==u && fa<fu))){
-                while(a-- > 0) vowelOrder.append('a');
-            }
-            else if(e>0 && (e>a || (e==a && fe<fa)) && (e>i || (e==i && fe<fi)) && (e>o || (e==o && fe<fo)) && (e>u || (e==u && fe<fu))){
-                while(e-- > 0) vowelOrder.append('e');
-            }
-            else if(i>0 && (i>a || (i==a && fi<fa)) && (i>e || (i==e && fi<fe)) && (i>o || (i==o && fi<fo)) && (i>u || (i==u && fi<fu))){
-                while(i-- > 0) vowelOrder.append('i');
-            }
-            else if(o>0 && (o>a || (o==a && fo<fa)) && (o>e || (o==e && fo<fe)) && (o>i || (o==i && fo<fi)) && (o>u || (o==u && fo<fu))){
-                while(o-- > 0) vowelOrder.append('o');
-            }
-            else if(u>0){
-                while(u-- > 0) vowelOrder.append('u');
+                if (!firstIndex.containsKey(ch)) {
+                    firstIndex.put(ch, i);
+                }
             }
         }
 
-        StringBuilder sb = new StringBuilder();
+        // sort
+        Collections.sort(list, (a, b) -> {
+            if (!freq.get(a).equals(freq.get(b))) {
+                return freq.get(b) - freq.get(a);
+            }
+            return firstIndex.get(a) - firstIndex.get(b);
+        });
+
+        // put back
+        StringBuilder res = new StringBuilder(s);
         int idx = 0;
 
-        for(int k=0;k<s.length();k++){
-            char ch = s.charAt(k);
-
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-                sb.append(vowelOrder.charAt(idx));
-                idx++;
-            } else {
-                sb.append(ch);
+        for (int i = 0; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))) {
+                res.setCharAt(i, list.get(idx++));
             }
         }
 
-        return sb.toString();
+        return res.toString();
+    }
+
+    private boolean isVowel(char ch) {
+        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
     }
 }

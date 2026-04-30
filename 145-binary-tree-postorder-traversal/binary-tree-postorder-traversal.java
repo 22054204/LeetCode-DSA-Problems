1aab06ae917a2a1b01fe55d137da8d19f2f5ef01
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-04-30 18:42:51 +0530

    Time: 1 ms (11.85%) | Memory: 42.9 MB (84.27%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,23 +15,27 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-      // postorder = LRN -> Left Right Node
+        // NRL then reverse → LRN
         List<Integer> result = new ArrayList<>();
-        Stack<TreeNode> st = new Stack<>();
-        TreeNode lastVisited = null;
-        while(root!=null || !st.isEmpty()){
-            while(root != null){
-                st.push(root);
-                root = root.left;
-            }
-            TreeNode node = st.peek();
-            if(node.right != null && lastVisited != node.right){
-                root = node.right;
-            } else {
-                result.add(node.val);
-                lastVisited = st.pop();
+        if(root == null) return result;
+
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        st1.push(root);
+
+        while(!st1.isEmpty()){
+            TreeNode node = st1.pop();
+            st2.push(node);
+
+            if(node.left != null) st1.push(node.left);
+            if(node.right != null) st1.push(node.right);
+
+            while(!st2.isEmpty()){
+                result.add(st2.pop().val);
             }
         }
+
+        Collections.reverse(result);
         return result;
     }
 }

17e91e3c21b66b23216498b3bc6a073c0468059f
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-04 07:55:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (51.87%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+
+        for(int i = 0; i < n/2; i++){
+            for(int j = i; j < n - i - 1; j++){
+
+                int temp = matrix[i][j];
+
+                matrix[i][j] = matrix[n-1-j][i];
+
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+
+                matrix[j][n-1-i] = temp;
+            }
+        }
+    }
+}
+
+/*
+IDEA:- rotate 4 positions at once
+top → right
+right → bottom
+bottom → left
+left → top
+*/
+/*
+MAPPING RULE :-
+(i , j)
+(j , n-1-i)
+(n-1-i , n-1-j)
+(n-1-j , i)
+*/

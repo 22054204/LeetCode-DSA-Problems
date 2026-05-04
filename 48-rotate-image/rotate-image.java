16be60160238438531dd4c913c4100df6fc5a30f
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-04 07:59:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (16.04%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,29 +1,28 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for(int i = 0; i < n/2; i++){
-            for(int j = i; j < n - i - 1; j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[n-1-j][i];
-                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
-                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
-                matrix[j][n-1-i] = temp;
+    public void rotate(int[][] mat) {
+        transpose(mat, 0, mat.length-1);
+        reverse(mat, 0);
+    }
+    public void transpose(int[][] mat, int i, int j){
+        for(i=0;i<mat.length;i++){
+            for(j=i;j<mat[0].length;j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+    }
+    public void reverse(int[][] mat, int i){
+        for(i=0;i<mat.length;i++){
+            int left = 0;
+            int right = mat.length-1;
+            while(left<right){
+                int temp = mat[i][left];
+                mat[i][left] = mat[i][right];
+                mat[i][right] = temp;
+                left++;
+                right--;
             }
         }
     }
 }
-
-/*
-IDEA:- rotate 4 positions at once
-top → right
-right → bottom
-bottom → left
-left → top
-*/
-/*
-MAPPING RULE :-
-(i , j)
-(j , n-1-i)
-(n-1-i , n-1-j)
-(n-1-j , i)
-*/

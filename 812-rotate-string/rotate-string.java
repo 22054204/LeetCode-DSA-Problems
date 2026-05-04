44cbf99383831acefc4969bf3a548924c54733a2
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-04 10:42:36 +0530

    Time: 1 ms (23.63%) | Memory: 43.3 MB (8.48%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,8 +1,37 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+        return BruteForce(s, goal);
+    }
+    public boolean Optimal(String s, String goal) {
         if(s.length()!=goal.length()) return false;
         String temp = s+s;
         if(temp.contains(goal)) return true;
         return false;
+    }
+    public boolean BruteForce(String s, String goal) {
+        if(s.length()!=goal.length()) return false;
+        if(s.equals(goal)) return true;
+        return helper(s, goal);
+    }
+    private static boolean helper(String s, String goal){
+        int n = s.length();
+        for(int k = 1;k<=n;k++){
+            char[] arr1 = s.toCharArray();
+            char[] arr2 = goal.toCharArray();
+            rotate(arr1, 0, n-1);
+            rotate(arr1, 0, k-1);
+            rotate(arr1, k, n-1);
+            if(Arrays.equals(arr1, arr2)) return true;
+        }
+        return false;
+    }
+    private static void rotate(char[] arr1, int i, int j){
+        while(i<j){
+            char temp = arr1[i];
+            arr1[i] = arr1[j];
+            arr1[j] = temp;
+            i++;
+            j--;
+        }
     }
 }

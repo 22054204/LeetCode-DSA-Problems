6da42627a3ada61b59ceb6e3fc33eb367ef5fcad
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-08 21:01:25 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (37.91%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -7,25 +7,26 @@ class Solution {
     private static int BS(int[] nums, int n){
         int start = 0;
         int end = n-1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            int leftLen = mid-start;
-            int rightLen = end-mid;
-            if((mid == 0 || nums[mid] != nums[mid-1]) && (mid == n-1 || nums[mid] != nums[mid+1])){
-                return nums[mid];
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            int leftLen = mid - start;
+            int rightLen = end - mid;
+            if(mid < n-1 && nums[mid] == nums[mid+1]){
+                if(leftLen % 2 == 0){
+                    start = mid + 2;
+                }else{
+                    end = mid - 1;
+                }
             }
-            if(mid < n-1 && nums[mid]==nums[mid+1]){
-                if(leftLen%2==0){
-                    start = mid+2;
+            else if(mid > 0 && nums[mid] == nums[mid-1]){
+                if(rightLen % 2 == 0){
+                    end = mid - 2;
                 }else{
-                    end = mid-1;
+                    start = mid + 1;
                 }
-            }else if(mid > 0 && nums[mid]==nums[mid-1]){
-                if(rightLen%2==0){
-                    end = mid-2;
-                }else{
-                    start = mid+1;
-                }
+            }
+            else{
+                return nums[mid];
             }
         }
         return -1;

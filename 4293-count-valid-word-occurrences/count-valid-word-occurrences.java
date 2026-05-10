ec3d37712c5bf790d5251dcc825d60b08cd36062
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-10 21:04:21 +0530

    Time: 1148 ms (5.00%) | Memory: 131.3 MB (13.92%) - LeetSync

--- 4293-count-valid-word-occurrences/count-valid-word-occurrences.java
+++ 4293-count-valid-word-occurrences/count-valid-word-occurrences.java
@@ -1,30 +1,48 @@
 class Solution {
     public int[] countWordOccurrences(String[] chunks, String[] queries) {
         String s = "";
-        for(int i=0;i<chunks.length;i++){
+        for(int i = 0; i < chunks.length; i++){
             s += chunks[i];
         }
         int n = queries.length;
-
-        s = s.replaceAll("[^a-z-]", " ");
-        s = s.replaceAll("--+", " ");
-        s = s.replaceAll("(^-|-$)", " ");
-        s = s.replaceAll("- ", " ");
-        s = s.replaceAll(" -", " ");
-
-        String[] arr = s.split("\\s+");
-
+        ArrayList<String> list = new ArrayList<>();
+        String temp = "";
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch >= 'a' && ch <= 'z'){
+                temp += ch;
+            }
+            else if(ch == '-'){
+                if(i > 0 && i < s.length() - 1 &&
+                   s.charAt(i - 1) >= 'a' && s.charAt(i - 1) <= 'z' &&
+                   s.charAt(i + 1) >= 'a' && s.charAt(i + 1) <= 'z'){
+                    temp += ch;
+                }
+                else{
+                    if(temp.length() > 0){
+                        list.add(temp);
+                        temp = "";
+                    }
+                }
+            }
+            else{
+                if(temp.length() > 0){
+                    list.add(temp);
+                    temp = "";
+                }
+            }
+        }
+        if(temp.length() > 0){
+            list.add(temp);
+        }
         HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(arr[i].length() == 0) continue;
-            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        for(int i = 0; i < list.size(); i++){
+            String word = list.get(i);
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
         int[] result = new int[n];
-        for(int i=0;i<n;i++){
-            if(map.containsKey(queries[i])){
-                result[i] = map.get(queries[i]);
-            }
-            else result[i] = 0;
+        for(int i = 0; i < n; i++){
+            result[i] = map.getOrDefault(queries[i], 0);
         }
         return result;
     }

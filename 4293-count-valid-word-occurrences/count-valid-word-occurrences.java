29c89c14114061f3fe545a44005034d1ee10a5e0
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-10 21:10:49 +0530

    Time: 702 ms (6.46%) | Memory: 129.3 MB (16.35%) - LeetSync

--- 4293-count-valid-word-occurrences/count-valid-word-occurrences.java
+++ 4293-count-valid-word-occurrences/count-valid-word-occurrences.java
@@ -1,31 +1,32 @@
 class Solution {
     public int[] countWordOccurrences(String[] chunks, String[] queries) {
         String s = "";
-        for(int i=0;i<chunks.length;i++){
+
+        for(int i=0; i<chunks.length; i++){
             s += chunks[i];
         }
+
         int n = queries.length;
 
         s = s.replaceAll("[^a-z-]", " ");
-        s = s.replaceAll("--+", " ");
-        s = s.replaceAll("(^-|-$)", " ");
-        s = s.replaceAll("- ", " ");
-        s = s.replaceAll(" -", " ");
+        s = s.replaceAll("(?<![a-z])-|-(?![a-z])", " ");
 
         String[] arr = s.split("\\s+");
 
         HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(arr[i].length() == 0) continue;
-            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
-        }
-        int[] result = new int[n];
-        for(int i=0;i<n;i++){
-            if(map.containsKey(queries[i])){
-                result[i] = map.get(queries[i]);
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].length() > 0){
+                map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
             }
-            else result[i] = 0;
         }
+
+        int[] result = new int[n];
+
+        for(int i=0; i<n; i++){
+            result[i] = map.getOrDefault(queries[i], 0);
+        }
+
         return result;
     }
 }

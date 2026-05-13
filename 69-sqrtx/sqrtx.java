3ccfe29e0f79a9e7730eb2b988865bcae63a87e0
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-13 11:53:55 +0530

    Time: 1 ms (97.92%) | Memory: 42.8 MB (23.19%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,56 @@
+// using Binary Search
+class Solution{
+    public int mySqrt(int x){
+        if (x < 2) return x;
+        int start = 1;
+        int end = x; // you also take end = x/2 , because its obvious ... like x = 9 , root9 <= 9/2;  So searching above x/2 is guaranteed useless...
+        int ans = 1;
+        while(start <= end){
+            int mid = start+(end-start)/2;
+            if(mid <= x/mid){ // you might write (mid <= x/mid) this ... because for bigger value of x this (mid*mid <= x) may overflow
+                ans = mid;
+                start = mid+1;
+            }else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
+
+/*
+// linear Search approach
+class Solution{
+    public int mySqrt(int x){
+        if(x<2) return x;
+        long i=1;
+        while(i*i <= x){
+            i++;
+        }
+        return (int) i-1;
+    }
+}
+*/
+
+/*
+//Newton’s Method
+class Solution{
+    public int mySqrt(int x){
+        if(x<2) return x;
+        long a = x;
+        while(a*a>x){
+            a = (a + x/a) / 2;
+        }
+        return (int) a;
+    }
+}
+*/
+
+/* But you cannot use inbuild Math.sqrt(), This is cheating
+class Solution {
+    public int mySqrt(int x) {
+        int number = (int) Math.sqrt(x);
+        return number;
+    }
+}
+*/

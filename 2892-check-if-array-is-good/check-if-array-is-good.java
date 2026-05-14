d0593974cf1966bc5e29d23d11fb4d5673f8f441
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-14 14:32:52 +0530

    Time: 3 ms (55.24%) | Memory: 44.6 MB (78.29%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,22 +1,33 @@
 class Solution {
-    public boolean isGood(int[] nums) {
+    public boolean isGood(int[] nums) { //[1,2,3,4,5,5,6,7]
         int max = findMax(nums);
+        if(nums.length!=max+1) return false;
+        if(maxCount(nums)<2) return false;
         List<Integer> list = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
             list.add(nums[i]);
         }
-        if(list.size()!=max+1) return false;
-        Collections.sort(list); //[1,2,3,4,5,5,6,7]
-        if(nums[nums.length-1]!=nums[nums.length-2]) return false;
         for(int i=1;i<=max;i++){
-            if(!list.contains(i)){
-                return false;
-            }
+            if(!list.contains(i)) return false;
         }
         return true;
     }
     private static int findMax(int[] nums){
-        Arrays.sort(nums);
-        return nums[nums.length-1];
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            max = Math.max(max, nums[i]);
+        }
+        if(max==Integer.MIN_VALUE) return -1;
+        return max;
+    }
+    private static int maxCount(int[] nums){
+        int count = 0;
+        int num = findMax(nums);
+        for(int i=0;i<nums.length;i++){
+            if(num==nums[i]){
+                count++;
+            }
+        }
+        return count;
     }
 }

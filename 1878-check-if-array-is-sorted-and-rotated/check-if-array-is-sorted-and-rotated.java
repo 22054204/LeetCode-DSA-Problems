636b3f5acc99c89c4eef7311bc1dc464e66df8a2
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-23 12:40:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (94.96%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,5 +1,21 @@
 class Solution {
     public boolean check(int[] nums) {
+        return optimal(nums);
+    }
+    private boolean optimal(int[] nums) {
+        int n = nums.length;
+        int count=0;
+        for (int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n]){
+                count++;
+            }
+        }
+        if(count>1){
+            return false;
+        }
+        return true;
+    }
+    private boolean BruteForce(int[] nums) {
         int[] arr = nums.clone();
         Arrays.sort(arr);
         int k = 0;
@@ -15,14 +31,14 @@ class Solution {
         }
         return false;
     }
-    public void rotate(int[] nums, int k) {
+    private void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k%n;
         swap(nums, 0, n-1);
         swap(nums, 0, k-1);
         swap(nums, k, n-1);
     }
-    public static void swap(int[] nums, int i, int j){
+    private static void swap(int[] nums, int i, int j){
         while(i<j){
             int temp = nums[i];
             nums[i] = nums[j];

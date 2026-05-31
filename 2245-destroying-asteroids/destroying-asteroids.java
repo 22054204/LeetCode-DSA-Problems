b254c68bb616a741e552c6b108914c05da7a83da
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-31 16:25:34 +0530

    Time: 29 ms (7.62%) | Memory: 107.5 MB (47.81%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(asteroids);
         for(int i=0;i<asteroids.length;i++){
             if(asteroids[i]>mass) return false;
-            else if(mass>=1e5) return true;
+            else if(mass>=Math.pow(10, 5)) return true;
             mass += asteroids[i];
         }
         return true;

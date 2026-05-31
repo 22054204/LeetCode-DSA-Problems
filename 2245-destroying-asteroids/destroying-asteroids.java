0609c2210591241654376f5ad999673ddf6420ec
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-05-31 16:24:03 +0530

    Time: 27 ms (31.18%) | Memory: 107.5 MB (30.95%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -1,10 +1,23 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) { // 3, 5, 9, 19, 21
+        return Method2(mass, asteroids);
+    }
+    public boolean Method1(int mass, int[] asteroids) {
         Arrays.sort(asteroids);
         long MASS = mass;
         for(int i=0;i<asteroids.length;i++){
             if(asteroids[i]>MASS) return false;
             MASS += asteroids[i];
+        }
+        return true;
+    }
+    //Method-2
+    public boolean Method2(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        for(int i=0;i<asteroids.length;i++){
+            if(asteroids[i]>mass) return false;
+            else if(mass>=Math.pow(10, 5)) return true;
+            mass += asteroids[i];
         }
         return true;
     }

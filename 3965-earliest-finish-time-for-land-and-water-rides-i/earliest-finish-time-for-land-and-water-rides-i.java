faa8630e79293e7a19d76ffca5c65b6f853982ce
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-02 22:03:14 +0530

    Time: 1 ms (100.00%) | Memory: 46.9 MB (61.51%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,105 +1,23 @@
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int landFirst = calc(landStartTime, landDuration, waterStartTime, waterDuration);
+        int waterFirst = calc(waterStartTime, waterDuration, landStartTime, landDuration);
+        return Math.min(landFirst, waterFirst);
+    }
+
+    private int calc(int[] start1, int[] dur1, int[] start2, int[] dur2) {
+        int minEnd = Integer.MAX_VALUE;
+
+        for(int i=0; i<start1.length; i++){
+            minEnd = Math.min(minEnd, start1[i] + dur1[i]);
+        }
+
         int ans = Integer.MAX_VALUE;
 
-        int[][] water = new int[waterStartTime.length][2];
-        for(int i=0; i<waterStartTime.length; i++){
-            water[i][0] = waterStartTime[i];
-            water[i][1] = waterDuration[i];
-        }
-
-        Arrays.sort(water, (a,b) -> a[0] - b[0]);
-
-        int m = water.length;
-        int[] prefWaterDur = new int[m];
-        int[] suffWaterFinish = new int[m];
-
-        prefWaterDur[0] = water[0][1];
-        for(int i=1; i<m; i++){
-            prefWaterDur[i] = Math.min(prefWaterDur[i-1], water[i][1]);
-        }
-
-        suffWaterFinish[m-1] = water[m-1][0] + water[m-1][1];
-        for(int i=m-2; i>=0; i--){
-            suffWaterFinish[i] = Math.min(suffWaterFinish[i+1], water[i][0] + water[i][1]);
-        }
-
-        for(int i=0; i<landStartTime.length; i++){
-            int lf = landStartTime[i] + landDuration[i];
-
-            int idx = upperBoundWater(water, lf);
-
-            if(idx > 0){
-                ans = Math.min(ans, lf + prefWaterDur[idx-1]);
-            }
-
-            if(idx < m){
-                ans = Math.min(ans, suffWaterFinish[idx]);
-            }
-        }
-
-        int[][] land = new int[landStartTime.length][2];
-        for(int i=0; i<landStartTime.length; i++){
-            land[i][0] = landStartTime[i];
-            land[i][1] = landDuration[i];
-        }
-
-        Arrays.sort(land, (a,b) -> a[0] - b[0]);
-
-        int n = land.length;
-        int[] prefLandDur = new int[n];
-        int[] suffLandFinish = new int[n];
-
-        prefLandDur[0] = land[0][1];
-        for(int i=1; i<n; i++){
-            prefLandDur[i] = Math.min(prefLandDur[i-1], land[i][1]);
-        }
-
-        suffLandFinish[n-1] = land[n-1][0] + land[n-1][1];
-        for(int i=n-2; i>=0; i--){
-            suffLandFinish[i] = Math.min(suffLandFinish[i+1], land[i][0] + land[i][1]);
-        }
-
-        for(int i=0; i<waterStartTime.length; i++){
-            int wf = waterStartTime[i] + waterDuration[i];
-
-            int idx = upperBoundLand(land, wf);
-
-            if(idx > 0){
-                ans = Math.min(ans, wf + prefLandDur[idx-1]);
-            }
-
-            if(idx < n){
-                ans = Math.min(ans, suffLandFinish[idx]);
-            }
+        for(int i=0; i<start2.length; i++){
+            ans = Math.min(ans, Math.max(minEnd, start2[i]) + dur2[i]);
         }
 
         return ans;
-    }
-
-    private int upperBoundWater(int[][] arr, int target){
-        int l = 0, r = arr.length;
-        while(l < r){
-            int mid = l + (r - l) / 2;
-            if(arr[mid][0] <= target){
-                l = mid + 1;
-            }else{
-                r = mid;
-            }
-        }
-        return l;
-    }
-
-    private int upperBoundLand(int[][] arr, int target){
-        int l = 0, r = arr.length;
-        while(l < r){
-            int mid = l + (r - l) / 2;
-            if(arr[mid][0] <= target){
-                l = mid + 1;
-            }else{
-                r = mid;
-            }
-        }
-        return l;
     }
 }

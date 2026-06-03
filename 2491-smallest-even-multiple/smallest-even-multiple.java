ab40c3ae5069ed3c32f81b6fdf5a3468af8bb1ee
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-03 09:05:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (49.12%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.java
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -1,11 +1,10 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        if(n==1) return 2;
         for(int i=1;i<=n*n;i++){
             if(i%2==0 && i%n==0){
                 return i;
             }
         }
-        return -1;
+        return n*2;
     }
 }

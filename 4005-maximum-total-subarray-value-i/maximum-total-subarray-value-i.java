91d9cc6d0a03b46d86ed6b9490c65f5edf472bff
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-09 21:45:46 +0530

    Time: 1 ms (100.00%) | Memory: 62.4 MB (20.14%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
@@ -1,5 +1,17 @@
 class Solution {
     public long maxTotalValue(int[] nums, int k) {
+        return Optimal(nums, k);
+    }
+    public long Optimal(int[] nums, int k) {
+        long max = nums[0];
+        long min = nums[0];
+        for(int i=0;i<nums.length;i++){
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
+        }
+        return (max-min)*k;
+    }
+    public long BruteForce(int[] nums, int k) {
         long ans = 0;
         long max = max(nums);
         long min = min(nums);

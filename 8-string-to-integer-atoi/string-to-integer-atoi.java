d5a533c48a853e0ada1367b8954f74d0c93fcfc6
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-12 01:21:34 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (12.72%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,11 +1,8 @@
 class Solution {
     public int myAtoi(String s) {
         if(s.length()==0) return 0;
-
         s = s.trim();
         if(s.length()==0) return 0;
-
-        StringBuilder sb = new StringBuilder();
 
         int x = 0;
         int sign = 1;
@@ -14,7 +11,6 @@ class Solution {
             x = 1;
         }else if(s.charAt(0)=='-'){
             sign = -1;
-            sb.append(s.charAt(0));
             x = 1;
         }
 
@@ -27,30 +23,22 @@ class Solution {
                 idx = i;
                 break;
             }
-            if(i==s.length()-1) return 0;
+            if(idx==s.length()-1) return 0;
         }
-
+        long res = 0;
         for(int i=idx;i<s.length();i++){
             if(s.charAt(i)<'0' || s.charAt(i)>'9'){
                 break;
             }
-            sb.append(s.charAt(i));
+            res = res*10 + (s.charAt(i)-'0');
+            if(sign==1 && res>Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }
+            if(sign==-1 && -res<Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
         }
 
-        String abc = sb.toString();
-
-        if(abc.length()==0 || abc.equals("-")) return 0;
-
-        try{
-            long res = Long.parseLong(abc);
-
-            if(res>Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if(res<Integer.MIN_VALUE) return Integer.MIN_VALUE;
-
-            return (int)res;
-        }catch(Exception e){
-            if(sign==-1) return Integer.MIN_VALUE;
-            return Integer.MAX_VALUE;
-        }
+        return (int)(sign*res);
     }
 }

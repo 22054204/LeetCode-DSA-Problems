eac508b71dd9fa11af3d00096396966136106adc
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-13 11:52:19 +0530

    Time: 9 ms (5.58%) | Memory: 47.4 MB (6.44%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.java
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -1,12 +1,12 @@
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
         String s = "";
-
-        char[] arr1 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-
+        
         HashMap<Character, Integer> map = new HashMap<>();
-        for(int i=0;i<arr1.length;i++){
-            map.put(arr1[i], i);
+        char ch = 'a';
+        for(int i=0;i<26;i++){
+            map.put(ch, i);
+            ch++;
         }
 
         for(int i=0;i<words.length;i++){
@@ -14,7 +14,7 @@ class Solution {
             for(int j=0;j<words[i].length();j++){
                 sum += weights[map.get(words[i].charAt(j))];
             }
-            s += arr1[25-(sum%26)];
+            s += (char)('z'-(sum%26));
         }
         return s;
     }

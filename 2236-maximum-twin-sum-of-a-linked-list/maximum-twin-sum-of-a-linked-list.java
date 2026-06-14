7745b67f9adf3ca3882419e8de29f16d88946b51
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-14 22:56:22 +0530

    Time: 18 ms (8.08%) | Memory: 115.6 MB (16.02%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -10,15 +10,19 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
+        return TrickToPassQues(head);
+    }
+    // Store linked list values in an ArrayList for O(1) index access.
+    private int TrickToPassQues(ListNode head) {
         List<Integer> list = new ArrayList<>();
         ListNode temp = head;
         while(temp!=null){
             list.add(temp.val);
             temp = temp.next;
         }
-        return helper(list);
+        return helper2(list);
     }
-    private static int helper(List<Integer> list){
+    private static int helper2(List<Integer> list){
         int n = list.size();
         int maxSum = 0;
         for(int i=0;i<n;i++){
@@ -31,5 +35,41 @@ class Solution {
             maxSum = Math.max(maxSum, a+b);
         }
         return maxSum;
+    }
+    //Cause TLE - because O(n*n)
+    public int BruteForce(ListNode head) {
+        int n = 0; // size of linked list
+        ListNode temp1 = head;
+        while(temp1!=null){
+            n++;
+            temp1 = temp1.next;
+        }
+        temp1 = head;
+        int i=0;
+        int maxSum = 0;
+        while(temp1!=null && i<n && i>=0 && i<=(n/2)-1){
+            int a = temp1.val;
+            int b = helper1(head, n-1-i);
+            maxSum = Math.max(maxSum, a+b);
+            i++;
+            temp1 = temp1.next;
+        }
+        return maxSum;
+    }
+    private static int helper1(ListNode head, int val){
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        ListNode temp = head;
+        while(temp!=null){
+            if(val==0){
+                return temp.val;
+            }
+            else{
+                val--;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return prev.val;
     }
 }

a1cdb2b574dc6dba2a2b853a53db19924b5a4498
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-16 09:28:10 +0530

    Time: 4 ms (76.47%) | Memory: 55 MB (80.88%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -1,29 +1,23 @@
 class Solution {
-    StringBuilder sb = new StringBuilder();
     public String processStr(String s) {
-        try{
-            for(int i=0;i<s.length();i++){
-                if(s.charAt(i)>='a' && s.charAt(i)<='z'){
-                    sb.append(s.charAt(i));
-                }else if(s.charAt(i)=='*'){
-                    if(!sb.isEmpty()){
-                        sb.deleteCharAt(sb.length()-1);
-                    }else{
-                        continue;
-                    }
-                }
-                else if(s.charAt(i)=='#'){
-                    sb.append(sb.toString());
-                }
-                else{
-                    sb.reverse();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+                sb.append(s.charAt(i));
+            }else if(s.charAt(i)=='*'){
+                if(!sb.isEmpty()){
+                    sb.deleteCharAt(sb.length()-1);
+                }else{
+                    continue;
                 }
             }
-            return sb.toString();
+            else if(s.charAt(i)=='#'){
+                sb.append(sb.toString());
+            }
+            else{
+                sb.reverse();
+            }
         }
-        catch(Exception e){
-            System.out.print(sb);
-            return "";
-        }
+        return sb.toString();
     }
 }

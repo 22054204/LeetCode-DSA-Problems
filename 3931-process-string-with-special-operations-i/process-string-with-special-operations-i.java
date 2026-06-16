666b48b8076989dd95ed425e02c4397002cb64ba
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-16 09:29:44 +0530

    Time: 3 ms (100.00%) | Memory: 55.5 MB (36.76%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -2,16 +2,17 @@ class Solution {
     public String processStr(String s) {
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)>='a' && s.charAt(i)<='z'){
-                sb.append(s.charAt(i));
-            }else if(s.charAt(i)=='*'){
+            char ch = s.charAt(i);
+            if(ch>='a' && ch<='z'){
+                sb.append(ch);
+            }else if(ch=='*'){
                 if(!sb.isEmpty()){
                     sb.deleteCharAt(sb.length()-1);
                 }else{
                     continue;
                 }
             }
-            else if(s.charAt(i)=='#'){
+            else if(ch=='#'){
                 sb.append(sb.toString());
             }
             else{

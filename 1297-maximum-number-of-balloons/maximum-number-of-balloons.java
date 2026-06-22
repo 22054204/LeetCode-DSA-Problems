3835280a00d21fd5332d098a26272d6540fd7f00
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-22 08:28:01 +0530

    Time: 5 ms (57.37%) | Memory: 43.3 MB (79.23%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,44 +1,39 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        List<Character> list = new ArrayList<>();
+        int[] freq = new int[5];
+
         for(int i=0;i<text.length();i++){
-            if(text.charAt(i)=='b'||text.charAt(i)=='a'||text.charAt(i)=='l'||text.charAt(i)=='l'||text.charAt(i)=='o'||text.charAt(i)=='o'||text.charAt(i)=='n'){
-                list.add(text.charAt(i));
+            char ch = text.charAt(i);
+            if(ch=='b'){
+                freq[0]++;
+            }
+            else if(ch=='a'){
+                freq[1]++;
+            }
+            else if(ch=='l'){
+                freq[2]++;
+            }
+            else if(ch=='o'){
+                freq[3]++;
+            }
+            else if(ch=='n'){
+                freq[4]++;
             }
         }
-        if(list.size()<7) return 0;
         int ans = 0;
-        while(!list.isEmpty()){
-            int count = 0;
-            if(list.contains('b')){
-                count++;
-                list.remove((Character) 'b');
+        while(true) {
+            if(freq[0] >= 1 && freq[1] >= 1 && freq[2] >= 2 && freq[3] >= 2 && freq[4] >= 1) {
+                freq[0]--;
+                freq[1]--;
+                freq[2] -= 2;
+                freq[3] -= 2;
+                freq[4]--;
+
+                ans++;
             }
-            if(list.contains('a')){
-                count++;
-                list.remove((Character) 'a');
+            else {
+                break;
             }
-            if(list.contains('l')){
-                count++;
-                list.remove((Character) 'l');
-            }
-            if(list.contains('l')){
-                count++;
-                list.remove((Character) 'l');
-            }
-            if(list.contains('o')){
-                count++;
-                list.remove((Character) 'o');
-            }
-            if(list.contains('o')){
-                count++;
-                list.remove((Character) 'o');
-            }
-            if(list.contains('n')){
-                count++;
-                list.remove((Character) 'n');
-            }
-            if(count==7) ans++;
         }
         return ans;
     }

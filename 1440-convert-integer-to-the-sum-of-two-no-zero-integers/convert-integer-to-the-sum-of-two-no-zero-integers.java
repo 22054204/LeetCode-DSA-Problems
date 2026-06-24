b4b787b22c1e7db50e4f6b66eb9eb04934dd4d09
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-24 07:05:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (86.54%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] getNoZeroIntegers(int n) {
-        int[] result = new int[2];
         for(int i=0;i<n;i++){
             if(isValid(i) && isValid(n-i)){
-                result[0] = i;
-                result[1] = n-i;
+                return new int[]{i, n-i};
             }
         }
-        return result;
+        return new int[]{};
     }
     private static boolean isValid(int num){
+        if(num==0) return false;
         while(num>0){
             if(num%10==0) return false;
             else num = num/10;

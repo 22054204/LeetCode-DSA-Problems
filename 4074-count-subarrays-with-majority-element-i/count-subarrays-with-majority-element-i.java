1b19c05c1012a208efaddd7d4ce14635273068b0
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-25 08:15:02 +0530

    Time: 51 ms (15.08%) | Memory: 46.8 MB (88.92%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.java
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.java
@@ -1,5 +1,8 @@
 class Solution {
     public int countMajoritySubarrays(int[] nums, int target) {
+        return Method2(nums, target);
+    }
+    public int Method2(int[] nums, int target) {
         int count = 0;
         for(int i=0;i<nums.length;i++){
             int freq = 0;
@@ -10,5 +13,28 @@ class Solution {
             }
         }
         return count;
+    }
+    // Brute Force - TLE for given constraints; suitable only for small arrays.
+    public int Method1(int[] nums, int target) {
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                List<Integer> list = new ArrayList<>();
+                for(int k=i;k<=j;k++){
+                    list.add(nums[k]);
+                }
+                if(isValid(list, target)) count++;
+            }
+        }
+        return count;
+    }
+    private static boolean isValid(List<Integer> list, int target){
+        int freq = 0;
+        for(int i=0;i<list.size();i++){
+            if(list.get(i)==target){
+                freq++;
+            }
+        }
+        return freq>list.size()/2;
     }
 }

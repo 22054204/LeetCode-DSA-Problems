f1db8bacf94b15f55c3f3b3c59804851dc019885
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-06-27 01:16:55 +0530

    Time: 5 ms (90.35%) | Memory: 47.1 MB (18.26%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.java
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.java
@@ -1,5 +1,30 @@
 class Solution {
     public int countMajoritySubarrays(int[] nums, int target) {
+        return Method5(nums, target);
+    }
+    public int Method5(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int cummSum = 0;
+        int validLeftPoints = 0;
+        int result = 0;
+
+        for(int i=0;i<nums.length;i++) {
+            if (nums[i] == target) {
+                validLeftPoints += map.getOrDefault(cummSum, 0);
+                cummSum += 1;
+            }else{
+                cummSum -= 1;
+                validLeftPoints -= map.getOrDefault(cummSum, 0);
+            }
+            map.put(cummSum, map.getOrDefault(cummSum, 0) + 1);
+            result += validLeftPoints;
+        }
+        return result;
+    }
+    //by using cummulative sum 
+    public int Method4(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
                 nums[i] = 1;
@@ -26,5 +51,61 @@ class Solution {
             }
         }
         return result;
+    }
+    
+    // improved BruteForce
+    public int Method3(int[] nums, int target) {
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            int freq = 0;
+            for(int j=i;j<nums.length;j++){
+                if (nums[j] == target) freq++;
+                int len = j - i + 1;
+                if (freq > len / 2) count++;
+            }
+        }
+        return count;
+    }
+
+    // without generating all subarrays, but not works because O(n^3)
+    public int Method2(int[] nums, int target) {
+        int result = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                int count = 0;
+                for(int k=i;k<=j;k++){
+                    if(nums[k]==target) count++;
+                }
+                int size = j-i+1;
+                if(count>size/2){
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    // Brute Force - TLE for given constraints; suitable only for small arrays.
+    public int Method1(int[] nums, int target) {
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                List<Integer> list = new ArrayList<>();
+                for(int k=i;k<=j;k++){
+                    list.add(nums[k]);
+                }
+                if(isValid(list, target)) count++;
+            }
+        }
+        return count;
+    }
+    private static boolean isValid(List<Integer> list, int target){
+        int freq = 0;
+        for(int i=0;i<list.size();i++){
+            if(list.get(i)==target){
+                freq++;
+            }
+        }
+        return freq>list.size()/2;
     }
 }

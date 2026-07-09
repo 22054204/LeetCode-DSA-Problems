53404c0a3c13764bd276dc0d5ae0432c63511ee8
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-09 14:14:12 +0530

    Time: 23 ms (42.72%) | Memory: 124.5 MB (91.26%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.java
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -1,5 +1,11 @@
+import java.math.*;
 class Solution {
     public int[] sumAndMultiply(String s, int[][] queries) {
+        return Optimal(s, queries);
+    }
+
+    // Optimal 
+    public int[] Optimal(String s, int[][] queries) {
         int MOD = 1000000007;
         int[] prefixSum = new int[s.length()];
         prefixSum[0] = s.charAt(0) - '0';
@@ -27,9 +33,9 @@ class Solution {
         }
         //System.out.println("nonZeros "+Arrays.toString(nonZeros));
 
-        int[] power10 = new int[s.length()+1];
+        int[] power10 = new int[s.length()];
         power10[0] = 1;
-        for(int i=1;i<=s.length();i++){
+        for(int i=1;i<s.length();i++){
             power10[i] = (int)(10L*power10[i-1]%MOD);
         }
         //System.out.println("power10 "+Arrays.toString(power10));
@@ -55,16 +61,80 @@ class Solution {
         }
         return result;
     }
+
+    // using BIGINTEGER - TLE
+    public int[] Better(String s, int[][] queries) {
+        BigInteger[] arr = new BigInteger[queries.length];
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            String dummy = s.substring(a, b+1);
+            BigInteger num = new BigInteger(dummy);
+            arr[i] = num;
+        }
+        for (int i = 0; i < ans.length; i++) {
+            BigInteger num = helper2(arr[i]);
+            BigInteger mod = BigInteger.valueOf(1000000007L);
+            ans[i] = num.mod(mod).intValue();
+        }
+        return ans;
+    }
+    public BigInteger helper2(BigInteger n) {
+        BigInteger rev = BigInteger.ZERO;
+        BigInteger sum = BigInteger.ZERO;
+        while (n.compareTo(BigInteger.ZERO) > 0) {
+            if (n.mod(BigInteger.TEN).equals(BigInteger.ZERO)) {
+                n = n.divide(BigInteger.TEN);
+            } else {
+                rev = rev.multiply(BigInteger.TEN).add(n.mod(BigInteger.TEN));
+                sum = sum.add(rev.mod(BigInteger.TEN));
+                n = n.divide(BigInteger.TEN);
+            }
+        }
+        BigInteger revToSt = BigInteger.ZERO;
+        while (rev.compareTo(BigInteger.ZERO) > 0) {
+            revToSt = revToSt.multiply(BigInteger.TEN).add(rev.mod(BigInteger.TEN));
+            rev = rev.divide(BigInteger.TEN);
+        }
+        return revToSt.multiply(sum);
+    }
+
+    // BruteForce Approach
+    public int[] BruteForce(String s, int[][] queries) {
+        long MOD = 1000000007L;
+        long[] arr = new long[queries.length];
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            String dummy = s.substring(a, b+1);
+            long num = Long.parseLong(dummy);
+            arr[i] = num;
+        }
+        for (int i = 0; i < ans.length; i++) {
+            long num = helper1(arr[i]);
+            ans[i] = (int) (num % MOD);
+        }
+        return ans;
+    }
+    public long helper1(long n) {
+        long rev = 0;
+        long sum = 0;
+        while (n > 0) {
+            if (n % 10 == 0) {
+                n /= 10;
+            } else {
+                rev = (rev * 10) + (n % 10);
+                sum += rev % 10;
+                n /= 10;
+            }
+        }
+        long revToSt = 0;
+        while (rev > 0) {
+            revToSt = (revToSt * 10) + (rev % 10);
+            rev /= 10;
+        }
+        return revToSt * sum;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-

99d94f4083587732c3c4d4c4dea264c5759c8946
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-15 19:43:19 +0530

    Time: 1085 ms (12.50%) | Memory: 206.7 MB (37.50%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
@@ -1,5 +1,9 @@
 class Solution {
     public int subsequencePairCount(int[] nums) {
+        return BottomUpDP(nums);
+    }
+    // bottom up DP Approach
+    public int BottomUpDP(int[] nums) {
         int n = nums.length;
         int maxEle = maxEle(nums);
         int[][][] t = new int[n+1][maxEle+1][maxEle+1];  
@@ -30,5 +34,19 @@ class Solution {
     }
     private static int gcd(int a, int b){
         return (b==0) ? a : gcd(b, a%b);
+    }
+
+    //Brute Force but without storing all subsequences ... because we only need gcd of it.
+    //588 / 622 testcases passed
+    public int BruteForce(int[] nums) {
+        return solve(nums, 0, 0, 0);
+    }
+    private static int solve(int[] nums, int i, int first, int second){
+        if(i==nums.length) return (first!=0 && second!=0 && first==second) ? 1 : 0;
+        
+        int skip = solve(nums, i+1, first, second);
+        int take1 = solve(nums, i+1, gcd(first, nums[i]), second);
+        int take2 = solve(nums, i+1, first, gcd(second, nums[i]));
+        return skip + take1 + take2;
     }
 }

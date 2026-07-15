69a8d9e1bf991c390d8b6d986fa47a4fe0ee526a
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-15 15:21:22 +0530

    Time: 5 ms (35.50%) | Memory: 41.9 MB (82.66%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,35 +1,19 @@
 class Solution {
     public int rotatedDigits(int n) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=1;i<=n;i++){
-            list.add(i);
-        }
         int ans = 0;
-        for(int i=0;i<list.size();i++){
-            int a = list.get(i);
-            if(a==2||a==5||a==6||a==9){
-                ans = ans + 1;
-            }else{
-                ans = ans + helper(a);
-            }
+        for(int i=1;i<=n;i++){
+            if(i==2||i==5||i==6||i==9) ans++;
+            else ans += helper(i);
         }
         return ans;
     }
     public static int helper(int a){
         int count = 0;
-        List<Integer> list = new ArrayList<>();
         while(a>0){
-            list.add(a%10);
+            int x = a%10;
+            if(x==3||x==4||x==7) return 0;
+            else if(x==2||x==5||x==6||x==9) count=1;
             a = a/10;
-        }
-        for(int i=0;i<list.size();i++){
-            int x = list.get(i);
-            if(x == 3 || x == 4 || x == 7){
-                return 0;
-            }
-            if(x==2||x==5||x==6||x==9){
-                count = 1;
-            }
         }
         return count;
     }

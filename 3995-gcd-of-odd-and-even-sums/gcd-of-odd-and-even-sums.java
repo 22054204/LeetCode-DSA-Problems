ed8a1e9bc20e2f3a158a756d64a9a89d69581f83
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-15 07:39:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (20.94%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,10 +1,36 @@
 class Solution {
     public int gcdOfOddEvenSums(int num) {
-        int sum = ((num+num)*((num+num)+1))/2;
-        int a = (sum+num)/2;
-        return gcd(a, sum-a);
+        return Method4(num);
     }
-    public static int gcd(int a, int b){
+    public int Method4(int num) {
+        return num;
+    }
+    public int Method3(int n) {
+        int odd = n*n;
+        int even = n*(n+1);
+        return n;
+    }
+    public int Method2(int n) {
+        int sumOdd = 0;
+        int sumEven = 0;
+        for(int i=1;i<=2*n;i++){
+            if(i%2==0){
+                sumEven = sumEven + i;
+            }
+            else{
+                sumOdd = sumOdd + i;
+            }
+        }
+        return gcd(sumOdd, sumEven);
+    }
+    public int Method1(int num) {
+        int n = num+num;
+        int sum = (n*(n+1))/2;
+        int b = (sum+num)/2;
+        int a = sum-b;
+        return gcd(a, b);
+    }
+    private static int gcd(int a, int b){
         if(b==0) return a;
         return gcd(b, a%b);
     }

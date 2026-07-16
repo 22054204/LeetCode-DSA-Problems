d93f712c499a1b79a0f091bcd698b350c12c94fe
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-16 22:24:10 +0530

    Time: 2 ms (91.08%) | Memory: 46.8 MB (58.09%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,8 +1,7 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        int max = findMax(nums);
-        int[] freq = new int[max+1];
+        int[] freq = new int[nums.length+1];
         for(int i=0;i<nums.length;i++){
             freq[nums[i]]++;
         }
@@ -20,12 +19,5 @@ class Solution {
             if(allZeros) break;
         }
         return result;
-    }
-    public static int findMax(int[] nums){
-        int max = 0;
-        for(int i=0;i<nums.length;i++){
-            max = Math.max(max, nums[i]);
-        }
-        return max;
     }
 }

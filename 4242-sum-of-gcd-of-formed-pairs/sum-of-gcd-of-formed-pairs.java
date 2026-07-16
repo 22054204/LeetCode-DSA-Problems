536fd2f9012557c31e4ee2a5f2f49ed5051c8bb9
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-16 07:30:15 +0530

    Time: 60 ms (32.50%) | Memory: 122.3 MB (38.50%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
@@ -1,22 +1,20 @@
 class Solution {
     public long gcdSum(int[] nums) {
-        long[] prefixGcd = new long[nums.length];
-        long curr_mxi = nums[0];
-        prefixGcd[0] = nums[0];
+        int curr_mxi = nums[0];
         for(int i=1;i<nums.length;i++){
             curr_mxi = Math.max(curr_mxi, nums[i]);
-            prefixGcd[i] = gcd(nums[i], curr_mxi);
+            nums[i] = gcd(nums[i], curr_mxi);
         }
-        Arrays.sort(prefixGcd);
+        Arrays.sort(nums);
         long sum = 0;
         int i=0;
-        int j=prefixGcd.length-1;
+        int j=nums.length-1;
         while(i<j){
-            sum+=gcd(prefixGcd[i++], prefixGcd[j--]);
+            sum+=gcd(nums[i++], nums[j--]);
         }
         return sum;
     }
-    private static long gcd(long a, long b){
+    private static int gcd(int a, int b){
         return (b==0)?a:gcd(b,a%b);
     }
 }

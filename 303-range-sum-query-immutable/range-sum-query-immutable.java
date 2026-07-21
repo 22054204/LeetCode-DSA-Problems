c090e9cc01b9399b1585b7a0882f4b1842da9558
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-21 18:41:44 +0530

    Time: 8 ms (71.40%) | Memory: 47.6 MB (80.77%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -12,7 +12,6 @@ class NumArray {
         return (left==0)?prefix[right]:prefix[right]-prefix[left-1];
     }
 }
-
 /* Method 2 -> 
     But slows than above solution because in this, we need to find out sum from scratch always (i.e from left to right)... but in above solution we firstly build Prefix Array and then for each query we got it's SUM in Constant Complexity O(1).
 
@@ -29,13 +28,4 @@ class NumArray {
             return sum;
         }
     }
-
 */ 
-
-
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * int param_1 = obj.sumRange(left,right);
- */

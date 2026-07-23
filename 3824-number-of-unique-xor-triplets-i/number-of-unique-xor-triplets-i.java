87772ba5767579ddbed0b58125e5765638a38d3a
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-23 10:07:46 +0530

    Time: 1 ms (51.02%) | Memory: 125.7 MB (10.20%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
@@ -4,6 +4,9 @@ class Solution {
         if(n==1) return 1;
         if(n==2) return 2;
         int bits=32-Integer.numberOfLeadingZeros(n);
+        //return (int)Math.pow(2, bits); 
+
+        // instead of using inbuild power function ... precompute powers of 2 up to the maximum.
         int[] pow={1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072};
         return pow[bits];
     }

4af25ae5fd35b5dd1cf951c0ea1aadc1c6fb7bce
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-23 15:49:28 +0530

    Time: 16 ms (24.09%) | Memory: 46.3 MB (75.79%) - LeetSync

--- 482-license-key-formatting/license-key-formatting.java
+++ 482-license-key-formatting/license-key-formatting.java
@@ -1,30 +1,19 @@
 class Solution {
     public String licenseKeyFormatting(String s, int k) {
-        int idx = 0;
-        boolean onlyHyp = false;
-        while(idx<s.length()){
-            if(s.charAt(idx)=='-'){
-                onlyHyp = true;
-            }
-            else {
-                onlyHyp = false;
-                break;
-            }
-            idx++;
-        }
-        if(onlyHyp) return "";
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)!='-') sb.append(s.charAt(i));
         }
-        sb.reverse();
         s = sb.toString().toUpperCase();
         StringBuilder res = new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(i%k==0) res.append('-');
+        int idx = 0;
+        for(int i=s.length()-1;i>=0;i--){
             res.append(s.charAt(i));
+            idx++;
+            if(idx%k==0) res.append('-');
         }
-        res.deleteCharAt(0);
+        if(res.length()==0) return "";
+        if(res.charAt(res.length()-1)=='-') res.deleteCharAt(res.length()-1);
         return res.reverse().toString();
     }
 }

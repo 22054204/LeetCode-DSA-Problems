ee7b275d921f3d1cba11f56fc18f9b313bf9a9cd
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-24 13:35:58 +0530

    Time: 60 ms (100.00%) | Memory: 46.8 MB (96.36%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.java
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.java
@@ -1,27 +1,54 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        boolean[] pair=new boolean[2048];
-        boolean[] ans=new boolean[2048];
-        int n=nums.length;
+        int maxVal = 0;
+        for (int x : nums) maxVal = Math.max(maxVal, x);
 
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                pair[nums[i]^nums[j]]=true;
-            }
+        int D = 1;
+        while (D <= maxVal) D <<= 1; // all XOR results stay < D
+
+        long[] f = new long[D];
+        for (int x : nums) f[x] = 1;      // presence indicator (dupes don't matter)
+
+        long[] Ff = f.clone();
+        fwht(Ff);
+
+        // ---- pairwise XOR-achievable set: conv(f, f) ----
+        long[] Fpair = new long[D];
+        for (int i = 0; i < D; i++) Fpair[i] = Ff[i] * Ff[i];
+        fwht(Fpair); // applying fwht again = inverse transform (up to scale D)
+
+        boolean[] pairSet = new boolean[D];
+        for (int i = 0; i < D; i++) {
+            if (Fpair[i] / D > 0) pairSet[i] = true;
         }
 
-        for(int i=0;i<n;i++){
-            for(int x=0;x<2048;x++){
-                if(pair[x]){
-                    ans[nums[i]^x]=true;
+        // ---- triple-wise XOR-achievable set: conv(pairSet, f) ----
+        long[] Fp = new long[D];
+        for (int i = 0; i < D; i++) Fp[i] = pairSet[i] ? 1 : 0;
+        fwht(Fp);
+
+        long[] Ftriple = new long[D];
+        for (int i = 0; i < D; i++) Ftriple[i] = Fp[i] * Ff[i];
+        fwht(Ftriple);
+
+        int count = 0;
+        for (int i = 0; i < D; i++) {
+            if (Ftriple[i] / D > 0) count++;
+        }
+        return count;
+    }
+
+    // In-place Walsh–Hadamard Transform (self-inverse up to a factor of n)
+    private void fwht(long[] a) {
+        int n = a.length;
+        for (int len = 1; len < n; len <<= 1) {
+            for (int i = 0; i < n; i += (len << 1)) {
+                for (int j = i; j < i + len; j++) {
+                    long u = a[j], v = a[j + len];
+                    a[j] = u + v;
+                    a[j + len] = u - v;
                 }
             }
         }
-
-        int cnt=0;
-        for(boolean b:ans){
-            if(b) cnt++;
-        }
-        return cnt;
     }
 }

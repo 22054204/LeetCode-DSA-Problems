832e27cd85998183f13d1672e6f45b7954f220e9
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-25 08:09:37 +0530

    Time: 1 ms (92.13%) | Memory: 43.7 MB (18.30%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.java
+++ 171-excel-sheet-column-number/excel-sheet-column-number.java
@@ -1,10 +1,24 @@
 class Solution {
     public int titleToNumber(String s) {
+        return Method2(s);
+    }
+    // instead of using pow() method ... me build an precompute an array with pow of 26 from minLenght == 1 to maxLength of String == 7, to got pow of 26 in O(1) complexity.
+    public int Method2(String s) {
         int ans = 0;
         int[] pow26 = {1,26,676,17576,456976,11881376,308915776}; //1 <= columnTitle.length <= 7
         int x = 0;
         for(int i=s.length()-1;i>=0;i--){
             int mul = pow26[x++];
+            ans += (((int)s.charAt(i)) - 64) * mul;
+        }
+        return ans;
+    }
+    // using inbuild pow() Method
+    public int Method1(String s) {
+        int ans = 0;
+        int x = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            int mul = (int)Math.pow(26, x++);
             ans += (((int)s.charAt(i)) - 64) * mul;
         }
         return ans;

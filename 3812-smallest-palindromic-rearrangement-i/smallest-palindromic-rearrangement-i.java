f6a497dcf3ec2097ce195c96fc8bbdf28ebd6428
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-28 14:29:34 +0530

    Time: 34 ms (36.95%) | Memory: 48.2 MB (38.15%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,16 +1,12 @@
 class Solution {
     public String smallestPalindrome(String s) {
         if(s.length()==1) return s;
-
         int[] freq = new int[26];
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
             freq[ch-'a']++;
         }
-        //System.out.println(Arrays.toString(freq));
-
         StringBuilder left = new StringBuilder();
-        StringBuilder right = new StringBuilder();
         StringBuilder middle = new StringBuilder();
 
         // help us to find left and middle
@@ -26,16 +22,6 @@ class Solution {
                 }
             }
         }
-
-        //help us to find right
-        for(int i=left.length()-1;i>=0;i--){
-            right.append(left.charAt(i));
-        }
-        
-        // System.out.println(left);
-        // System.out.println(middle);
-        // System.out.println(right);
-
-        return left.toString()+middle.toString()+right.toString();
+        return left.toString()+middle.toString()+left.reverse().toString();
     }
 }

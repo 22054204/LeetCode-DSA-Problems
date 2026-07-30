4eb5fe8f071f25afa9cc01d3f96fac833a435152
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-07-30 18:13:27 +0530

    Time: 9 ms (94.48%) | Memory: 48.3 MB (17.44%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -5,26 +5,10 @@ class Solution {
             freq[word.charAt(i)-'a']++;
         }
         Arrays.sort(freq);
-        freq = reverse(freq);
         int count = 0;
-        for(int i=0;i<freq.length;i++){
-            count+=freq[i]*((i>=0&&i<=7)?1:(i>=8&&i<=15)?2:(i>=16&&i<=23)?3:4);
-                // if(i>=0 && i<=7) count+=freq[i]*1;
-                // else if(i>=8 && i<=15) count+=freq[i]*2;
-                // else if(i>=16 && i<=23) count+=freq[i]*3;
-                // else count+=freq[i]*4;
+        for(int i=25;i>=0;i--){
+            count+=freq[i]*((i>=18&&i<=25)?1:(i>=10&&i<=17)?2:(i>=2&&i<=9)?3:4);
         }
         return count;
-    }
-    private static int[] reverse(int[] nums){
-        int i=0,j=nums.length-1;
-        while(i<j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
-        return nums;
     }
 }

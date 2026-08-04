ca8a841be6b4c24ed89d2f18dee627bea37c1d3a
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-08-04 09:24:52 +0530

    Time: 1 ms (99.65%) | Memory: 43 MB (33.25%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,5 +1,21 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
+        return WithHeap(stones);
+    }
+    public int WithHeap(int[] stones) { //maxHeap requires to smashing the two heaviest stones
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<stones.length;i++){
+            maxHeap.offer(stones[i]);
+        }
+        while(maxHeap.size()>1){
+            int y = maxHeap.poll();
+            int x = maxHeap.poll();
+            int z =  y-x;
+            if(z!=0) maxHeap.offer(z);
+        }
+        return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+    }
+    public int WithoutUsingHeapDataStructures(int[] stones) {
         if(stones.length==1) return stones[0];
         List<Integer> list = new ArrayList<>();
         for(int num:stones){
@@ -7,14 +23,12 @@ class Solution {
         }
         Collections.sort(list);
         while(list.size()>1){
-            int y = list.get(list.size()-1);
-            int x = list.get(list.size()-2);
-            list.remove(list.size()-1);
-            list.remove(list.size()-1);
+            int y = list.remove(list.size()-1);
+            int x = list.remove(list.size()-1);
             int z = Math.abs(y-x);
             if(z!=0) list.add(z);
             Collections.sort(list);
         }
-        return list.isEmpty() ? 0 : list.get(0);
+        return list.isEmpty()?0:list.get(0);
     }
 }

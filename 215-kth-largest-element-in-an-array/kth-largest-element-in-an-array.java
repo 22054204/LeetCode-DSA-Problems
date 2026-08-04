2b43071beb7f9ccd09a3a1020f20677fa0b5374c
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-08-04 13:17:45 +0530

    Time: 69 ms (59.45%) | Memory: 74.5 MB (48.55%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,5 +1,18 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
+        return Method2(nums, k);
+    }
+    //with Heap
+    public int Method2(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int num:nums){
+            minHeap.offer(num);
+            if(minHeap.size()>k) minHeap.poll();
+        }
+        return minHeap.poll();
+    }
+    // we can also solve this way, but in question they said we need to solve without sorting.
+    public int Method1(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length-k];
     }

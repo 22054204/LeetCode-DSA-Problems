afaa6c07efb388c97fec7bd7779d0a5fa2cb894c
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-08-14 18:21:21 +0530

    Time: 1 ms (8.29%) | Memory: 43.7 MB (15.94%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -16,15 +16,34 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         //return bfs(root);
-        return recursiveDFS(root, root);
+        //return recursiveDFS(root, root);
+        return iterativeDFS(root);
     }
-    public boolean recursiveDFS(TreeNode root1, TreeNode root2) {
+    private boolean iterativeDFS(TreeNode root) {
+        if(root==null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode root1 = stack.pop();
+            TreeNode root2 = stack.pop();
+            if(root1==null&&root2==null) continue;
+            if(root1==null||root2==null) return false;
+            if(root1.val!=root2.val) return false;
+            stack.push(root1.left);
+            stack.push(root2.right);
+            stack.push(root1.right);
+            stack.push(root2.left);
+        }
+        return true;
+    }
+    private boolean recursiveDFS(TreeNode root1, TreeNode root2) {
         if(root1==null && root2==null) return true;
         if(root1==null || root2==null) return false;
         if(root1.val!=root2.val)  return false;
         return recursiveDFS(root1.left, root2.right) && recursiveDFS(root1.right, root2.left);
     }
-    public boolean bfs(TreeNode root) {
+    private boolean bfs(TreeNode root) {
         if(root==null) return true;
         Queue<TreeNode> q1 = new LinkedList<>();
         Queue<TreeNode> q2 = new LinkedList<>();

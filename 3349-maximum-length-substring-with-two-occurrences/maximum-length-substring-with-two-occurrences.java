96f48ff6d4cb8fe5597bb4bec6cec5e108470e90
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-08-14 10:14:14 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (52.56%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -1,5 +1,26 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
+        return Method2(s);
+    }
+    //(more optimized version than previous(Method 1) ... because in this we don't reset the whole freq array).
+    public int Method2(String s) { 
+        int n = s.length();
+        int i=0;
+        int j=0;
+        int maxLen = 0;
+        int[] freq = new int[26];
+        while(i<n&&j<n){
+            freq[s.charAt(j)-'a']++;
+            while(freq[s.charAt(j)-'a']>2){
+                freq[s.charAt(i)-'a']--;
+                i++;
+            }
+            j++;
+            maxLen = Math.max(j-i, maxLen);
+        }
+        return maxLen;
+    }
+    public int Method1(String s) {
         int n = s.length();
         int i=0;
         int j=0;

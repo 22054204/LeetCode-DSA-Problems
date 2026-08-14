76c9c76dd606a0e7a5d94c0e8c21aef3405fb175
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-08-14 18:07:34 +0530

    Time: 3 ms (0.48%) | Memory: 43.3 MB (94.01%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,12 +15,25 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return check(root, root);
-    }
-    public static boolean check(TreeNode root1, TreeNode root2){
-        if(root1==null && root2==null) return true;
-        if(root1==null || root2==null) return false;
-        if(root1.val!=root2.val)  return false;
-        return check(root1.left, root2.right) && check(root1.right, root2.left);
+        if(root==null) return true;
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.offer(root);
+        q2.offer(root);
+        
+        while(!q1.isEmpty()&&!q2.isEmpty()){
+            TreeNode root1 = q1.poll();
+            TreeNode root2 = q2.poll();
+            if(root1==null&&root2==null) continue;
+            if(root1==null||root2==null) return false;
+            if(root1.val!=root2.val) return false;
+            q1.offer(root1.left);
+            q1.offer(root1.right);
+            
+            q2.offer(root2.right);
+            q2.offer(root2.left);
+        }
+        
+        return true;
     }
 }

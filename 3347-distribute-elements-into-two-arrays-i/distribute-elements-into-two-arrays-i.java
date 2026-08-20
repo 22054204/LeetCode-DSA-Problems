2657ad4f64d717c8617dd2856b9399456bca67c4
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-08-20 09:42:28 +0530

    Time: 1 ms (98.08%) | Memory: 46.7 MB (42.79%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] resultArray(int[] nums) {
-        int[] arr1 = new int[nums.length];
-        int[] arr2 = new int[nums.length];
+        int n = nums.length;
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
         arr1[0] = nums[0];
         arr2[0] = nums[1];
         int i=2;
         int idx1 = 1;
         int idx2 = 1;
-        while(i<nums.length){
+        while(i<n){
             if(arr1[idx1-1]>arr2[idx2-1]){
                 arr1[idx1++] = nums[i++];
             }else{
@@ -16,11 +17,11 @@ class Solution {
         }
 
         int idx = 0;
-        for(i=0;i<arr1.length;i++){
+        for(i=0;i<n;i++){
             if(arr1[i]!=0) nums[idx++] = arr1[i];
         }
 
-        for(int j=0;j<arr2.length;j++){
+        for(int j=0;j<n;j++){
             if(arr2[j]!=0) nums[idx++] = arr2[j];
         }
         return nums;

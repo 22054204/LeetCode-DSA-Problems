9f9eca85365b325da7f4ade5e2e284b820efbccd
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-08-26 22:39:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (36.07%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,31 +1,14 @@
 class Solution {
-    int[] dp = new int[45+1];
-    
+    int[] dp = new int[46];
     public int climbStairs(int n) {
         Arrays.fill(dp, -1);
-        return memo(n);
-    }
-    int memo(int n){ //Memoization
-        if(n<0) return 0;
-        if(n==0){
-            return 1; 
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i=4;i<46;i++){
+            dp[i] = dp[i-1]+dp[i-2];
         }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int one_step = memo(n-1);
-        int two_step = memo(n-2);
-
-        return dp[n] = one_step + two_step;
-    }
-    int helper(int n){ //recursive approach (TLE)
-        if(n<0) return 0;
-        if(n==0){
-            return 1; 
-        }
-        int one_step = helper(n-1);
-        int two_step = helper(n-2);
-
-        return one_step + two_step;
+        return dp[n];
     }
 }

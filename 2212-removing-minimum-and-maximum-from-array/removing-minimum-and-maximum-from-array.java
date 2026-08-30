2bfb8d46ca6fee230e94d879ff4ad490fbb0a447
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-08-30 16:52:23 +0530

    Time: 10 ms (4.91%) | Memory: 82.8 MB (97.64%) - LeetSync

---
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -0,0 +1,79 @@
+class Solution {
+    public int minimumDeletions(int[] nums) {
+        int n = nums.length;
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        int min = nums[0];
+        int max = nums[0];
+
+        int minIdx = 0;
+        int maxIdx = 0;
+
+        for(int i=1;i<n;i++){
+            if(min>nums[i]){
+                min = nums[i];
+                minIdx = i;
+            }
+            if(max<nums[i]){
+                max = nums[i];
+                maxIdx = i;
+            }
+        }
+
+        boolean leftSideForMin = false;
+        boolean rightSideForMin = false;
+
+        boolean leftSideForMax = false;
+        boolean rightSideForMax = false;
+
+        int a = Math.min(n-minIdx, minIdx+1); //min
+        if(a==n-minIdx) rightSideForMin = true;
+        else leftSideForMin = true;
+
+        int b = Math.min(n-maxIdx, maxIdx+1); //max
+        if(b==n-maxIdx) rightSideForMax = true;
+        else leftSideForMax = true;
+
+        System.out.println(min + " " + max);
+        System.out.println(a + " " + b);
+
+        int c = 0;
+        if((leftSideForMin && rightSideForMax) || (leftSideForMax && rightSideForMin)){
+            c=a+b;
+        }
+        else if((leftSideForMin && leftSideForMax) || (rightSideForMin && rightSideForMax)){
+            c=Math.max(a, b);
+        }
+        boolean minOccur = false;
+        boolean maxOccur = false;
+
+        int i=0;
+        int leftLen = 1;
+        while(minOccur==false || maxOccur==false){
+            if(nums[i]==min) minOccur = true;
+            if(nums[i]==max) maxOccur = true;
+            if(minOccur && maxOccur) break;
+            else leftLen++;
+            i++;
+        }
+
+        minOccur = false;
+        maxOccur = false;
+
+        i=nums.length-1;
+        int rightLen = 1;
+        while(minOccur==false || maxOccur==false){
+            if(nums[i]==min) minOccur = true;
+            if(nums[i]==max) maxOccur = true;
+            if(minOccur && maxOccur) break;
+            else rightLen++;
+            i--;
+        }
+
+        System.out.println(leftLen + " " + rightLen);
+        int len = Math.min(leftLen, rightLen);
+
+        return Math.min(len, c);
+    }
+}

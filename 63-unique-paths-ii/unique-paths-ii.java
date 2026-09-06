5c7d143b38a7a3d9cf049e33f4d2edf508c2d67e
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-09-06 20:51:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (64.56%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,11 +1,12 @@
 class Solution {
     int m = 0;
     int n = 0;
-    int[][] dp = new int[101][101];
+    int[][] dp;
     public int uniquePathsWithObstacles(int[][] mat) {
         m = mat.length;
         n = mat[0].length;
-        if(mat[m-1][n-1]==1) return 0;
+        if(mat[m-1][n-1]==1) return 0; 
+        dp = new int[m][n];
         for(int[] row:dp){
             Arrays.fill(row, -1);
         }

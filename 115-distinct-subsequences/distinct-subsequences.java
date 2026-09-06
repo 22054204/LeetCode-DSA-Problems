40d626a1360e0b08f3785a20177b606df0de4808
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-09-06 16:40:31 +0530

    Time: 18 ms (76.88%) | Memory: 54.4 MB (50.85%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,3 +1,4 @@
+// DP -> Bottom Up Approach
 class Solution {
     int[][] dp;
     int m=0;
@@ -37,3 +38,38 @@ class Solution {
         return dp[0][0];
     }
 }
+
+
+/*
+    TopDown Approach -> Recursive+Memoization.
+    
+    class Solution {
+        int[][] dp = new int[1001][1001];
+        public int numDistinct(String s, String t) {
+            for (int[] row : dp) {
+                Arrays.fill(row, -1);
+            }
+            return solve(s, t, 0, 0);
+        }
+        public int solve(String s, String t, int i, int j){
+            //base case 
+            if(j==t.length()){ // j is reaches to end, which means all char matches 
+                return 1;
+            }
+            if(i==s.length()){ // i reaches to end only, which means not char matches from String s with String t
+                return 0;
+            }
+
+            if(dp[i][j]!=-1){
+                return dp[i][j];
+            }
+            if(s.charAt(i)==t.charAt(j)){
+                dp[i][j] = solve(s, t, i+1, j+1)+solve(s, t, i+1, j);
+            }else{
+                dp[i][j] = solve(s, t, i+1, j);
+            }
+            return dp[i][j];
+        }
+    }
+
+*/

f1c87b2bd65224abfeb88aa9a5ef412b59043e58
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-09-07 15:43:52 +0530

    Time: 1 ms (99.92%) | Memory: 45.5 MB (62.20%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -8,13 +8,13 @@ class Solution {
         if(m==1) return triangle.get(0).get(0);
         dp = new int[m][n];
         for(int[] row:dp){
-            Arrays.fill(row, Integer.MAX_VALUE);
+            Arrays.fill(row, 10001);
         }
         return solve(triangle, 0, 0);
     }
     public int solve(List<List<Integer>> triangle, int i, int j){
         if(i==m-1) return triangle.get(i).get(j);
-        if(dp[i][j]!=Integer.MAX_VALUE) return dp[i][j];
+        if(dp[i][j]!=10001) return dp[i][j];
         int down = triangle.get(i).get(j) + solve(triangle, i+1, j);
         int downAndRight = triangle.get(i).get(j) + solve(triangle, i+1, j+1);
         return dp[i][j] = Math.min(down, downAndRight);

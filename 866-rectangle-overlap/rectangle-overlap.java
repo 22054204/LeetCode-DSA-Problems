097d12ddd395c5edd9a7101dfc6afc157f00152c
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-09-14 16:12:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (33.30%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int a = Math.max(rec1[0], rec2[0]);
+        int b = Math.min(rec1[2], rec2[2]);
+        int c = Math.max(rec1[1], rec2[1]);
+        int d = Math.min(rec1[3], rec2[3]);
+        return b-a>0 && d-c>0;
+    }
+}

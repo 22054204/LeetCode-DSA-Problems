60b64f2477d840de2a425147e67620525db8b7df
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-09-26 11:30:02 +0530

    Time: 234 ms (5.43%) | Memory: 101 MB (7.24%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+        Map<String, String> map = new HashMap<>();
+        for(int i=0;i<knowledge.size();i++){
+            map.put(knowledge.get(i).get(0), knowledge.get(i).get(1));
+        }
+        System.out.println(map);
+        StringBuilder sb = new StringBuilder();
+        int i=0;
+        while(i<s.length()){
+            if(s.charAt(i)=='('){
+                int j=i+1;
+                StringBuilder search = new StringBuilder();
+                while(s.charAt(j)!=')'){
+                    search.append(s.charAt(j));
+                    j++;
+                }
+                sb.append(map.getOrDefault(search.toString(), "?"));
+                    // if(map.containsKey(search.toString())){
+                    //     sb.append(map.get(search.toString()));
+                    // }else{
+                    //     sb.append('?');
+                    // }
+                i=j+1;
+                System.out.println(search);
+            }
+            else{
+                sb.append(s.charAt(i));
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+}
